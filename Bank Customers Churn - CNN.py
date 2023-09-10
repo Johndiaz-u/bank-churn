b6cd7e6c0482dbfb17ab2fbe528ea8a9423cc417
John Díaz <johndiaz-u@hotmail.com>
2023-09-10 17:42:31 -0400

    Update Bank Customers Churn - CNN.py

--- Bank Customers Churn - CNN.py
+++ Bank Customers Churn - CNN.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[36]:
-
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -16,82 +13,20 @@ import datetime
 warnings.filterwarnings("ignore")
 get_ipython().run_line_magic('matplotlib', 'inline')
 
-
-# In[37]:
-
-
 data = pd.read_csv('C:/Users/Dominga Genao/OneDrive/Escritorio/Churn Modeling.csv')
 data.head()
 
-
-# In[38]:
-
-
 data.describe()
-
-
-# In[39]:
-
-
 data.info()
-
-
-# In[40]:
-
-
 data.columns
-
-
-# In[41]:
-
-
 data.dtypes
-
-
-# In[42]:
-
-
 data.value_counts()
-
-
-# In[43]:
-
-
 data.isnull().sum()
-
-
-# In[44]:
-
-
 data.isnull().any()
-
-
-# In[45]:
-
-
 data.shape
-
-
-# In[46]:
-
-
 data = data.apply(pd.to_numeric, errors='coerce')
-
-
-# In[47]:
-
-
 data= data.fillna(0)
-
-
-# In[48]:
-
-
 data.mean()
-
-
-# In[50]:
-
 
 campos = ['RowNumber','CustomerId','CreditScore','Age','Tenure','Balance','NumOfProducts','HasCrCard','IsActiveMember','EstimatedSalary','Exited']
 
@@ -99,49 +34,22 @@ campos = ['RowNumber','CustomerId','CreditScore','Age','Tenure','Balance','NumOf
 data[campos].corr()
 
 
-# In[51]:
-
-
 plt.figure(figsize=(16,13))
 sns.heatmap(data[campos].corr(), annot=True)
-
-
-# In[54]:
-
 
 data.hist(figsize=(18,15))
 plt.show()
 
-
-# In[55]:
-
-
 sns.scatterplot(x='Balance', y= 'CreditScore', data=data)
-
-
-# In[58]:
-
 
 sns.set_style("whitegrid") 
 mean_col = ['RowNumber','Gender','Age','Tenure','Balance','Exited']
 
 sns.pairplot(data[mean_col],palette='Accent')
 
-
-# In[59]:
-
-
 sns.relplot(x='Age', y= 'CreditScore', data=data)
 
-
-# In[60]:
-
-
 sns.jointplot(x='Balance', y= 'CreditScore', data=data)
-
-
-# In[61]:
-
 
 plt.style.use("ggplot")
 plt.figure(figsize=(14,8))
@@ -150,20 +58,12 @@ plt.ylabel('CreditScore')
 sns.kdeplot(data['Balance'],shade=True,color='blue')
 plt.show()
 
-
-# In[62]:
-
-
 plt.style.use("default")
 sns.barplot(x="Balance", y="CreditScore",data=data[179:190])
 plt.title("Balance vs CreditScore",fontsize=15)
 plt.xlabel("Balance")
 plt.ylabel("CreditScore")
 plt.show()
-
-
-# In[63]:
-
 
 plt.style.use("default")
 sns.barplot(x="EstimatedSalary", y="CreditScore",data=data[183:190])
@@ -172,20 +72,12 @@ plt.xlabel("EstimatedSalary")
 plt.ylabel("CreditScore")
 plt.show()
 
-
-# In[64]:
-
-
 plt.style.use("default")
 sns.barplot(x="Tenure", y="Balance",data=data[170:190])
 plt.title("Tenure vs Balance",fontsize=15)
 plt.xlabel("Tenure")
 plt.ylabel("Balance")
 plt.show()
-
-
-# In[65]:
-
 
 plt.style.use("default")
 sns.barplot(x="HasCrCard", y="NumOfProducts",data=data[160:190])
@@ -194,15 +86,7 @@ plt.xlabel("HasCrCard")
 plt.ylabel("NumOfProducts")
 plt.show()
 
-
-# In[66]:
-
-
 list_1=list(data.columns)
-
-
-# In[67]:
-
 
 list_cate=[]
 for i in list_1:
@@ -210,74 +94,36 @@ for i in list_1:
         list_cate.append(i)
 
 
-# In[68]:
-
-
 from sklearn.preprocessing import LabelEncoder
 le=LabelEncoder()
-
-
-# In[69]:
 
 
 for i in list_cate:
     data[i]=le.fit_transform(data[i])
 
-
-# In[70]:
-
-
 data
-
-
-# In[111]:
-
 
 #drop the columns as it is no longer required
 X = data.drop('Geography',axis=1)
 y = data['Geography']
 
-
-# In[112]:
-
-
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
-
-# In[113]:
-
 
 print(len(X_test))
 print(len(X_train))
 print(len(y_test))
 print(len(y_train))
 
-
-# In[114]:
-
-
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-
-# In[77]:
-
-
 pip install tensorflow
 
-
-# In[78]:
-
-
 pip install keras
-
-
-# In[134]:
-
 
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
@@ -313,10 +159,6 @@ model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy']
 # Resumen del modelo
 model.summary()
 
-
-# In[136]:
-
-
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 # Calcula el Mean Squared Error (MSE)
@@ -326,10 +168,5 @@ print(f'Mean Squared Error: {mse}')
 # Calcula el Mean Absolute Error (MAE)
 mae = mean_absolute_error(y_test, y_pred)
 print(f'Mean Absolute Error: {mae}')
-
-
-# In[ ]:
-
-
 
 
